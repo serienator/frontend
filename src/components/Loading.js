340662afd7085dd14e28b1077a22a35e1a86ed9a
Mariana Perez <mperez@thoughtworks.com>
2020-01-11 19:25:03 +0100

    #1 Add loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Spinner } from 'react-bootstrap';
+
 
 function Loading() {
-    return <div className='loader'></div>
+    return <div className="page-container">
+        <Spinner animation="border" role="status" size="lg" className="spinner">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
+    </div>
 }
 
-export default Loading
+export default Loading;
